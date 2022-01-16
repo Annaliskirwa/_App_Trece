4baf184485184790f35442d042cc4efb37728485
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-16 22:40:46 +0300

    Adding a local storage service

--- src/app/services/web-storage.service.ts
+++ src/app/services/web-storage.service.ts
@@ -11,5 +11,7 @@ export class WebStorageService {
     return window.localStorage.getItem(key);
   }
 
-  public set(){}
+  public set(key: string, value: string): void{
+    window.localStorage.setItem(key,value);
+  }
 }
