c764f3cf0aef9f53013f50dd3b6d56e759fe1a2a
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-20 09:24:03 +0300

    Testing the highlight text pipe

--- src/app/pipes/highlight-text.pipe.spec.ts
+++ src/app/pipes/highlight-text.pipe.spec.ts
@@ -1,8 +1,13 @@
 import { HighlightTextPipe } from './highlight-text.pipe';
 
 describe('HighlightTextPipe', () => {
+  const pipe = new HighlightTextPipe();
+
   it('create an instance', () => {
-    const pipe = new HighlightTextPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('wrap a simple string in a <span>', () => {
+    expect(pipe.transform('foobar', 'foo')).toBe('<span class="highlight-text">foo</span>bar')
   });
 });
