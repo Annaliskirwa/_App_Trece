05f04d0d7005a2e54957deacb924ccfd9b0975df
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-21 23:34:01 +0300

    fixing lint errors

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,7 +8,7 @@ describe('User List App',()=>{
         await page.getDashboard();
 
     });
-    it('should have aaaa header', async()=>{
+    it("should have aaaa header", async()=>{
         const text = await page.getHeaderText();
         expect(text).toBe('Active Users');
     });
@@ -18,25 +18,25 @@ describe('User List App',()=>{
 
     });
 
-    describe('filter Input', ()=>{
+    describe("filter Input", ()=>{
         let input: ElementFinder;
         let items: ElementArrayFinder;
 
         beforeEach(async()=>{
             input = page.getInput();
             items = page.getListItems();
-            await input.sendKeys('davis');
+            await input.sendKeys("davis");
         });
 
-        it('should filter the list on the user search', async()=>{
+        it("should filter the list on the user search", async()=>{
             expect(await items.count()).toBe(2);
         });
-        it('should highlight filter text', async()=>{
+        it("should highlight filter text", async()=>{
             (await items).forEach(item =>{
-                const span = item.element(by.css('span'));
-                expect(span.getAttribute('class')).toBe('highlight-text');
-            })
-        })
+                const span = item.element(by.css("span"));
+                expect(span.getAttribute("class")).toBe("highlight-text");
+            });
+        });
     });
 
 });
