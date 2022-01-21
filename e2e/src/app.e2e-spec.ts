a4cc7bbdeab0891637bc111e84e8e1a5c2338093
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-21 20:50:30 +0300

    e2e to count users on page load

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,12 @@ describe('User List App',()=>{
         const header = by.css('h2');
         const text = await element(header).getText();
         expect(text).toBe('Active Users');
-    })
+    });
+    it('should have 16 users on page load', async()=>{
+        const items = by.css('li');
+        const users = element.all(items);
+        expect(await users.count()).toBe(16);
+
+    });
 
 });
