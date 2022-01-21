ecebf510d9fd8efcad75ec1adf839436c9241bd3
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-21 23:39:36 +0300

    target js,ts and jsx files in src directory

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,16 +2,18 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const { SpecReporter, StacktraceOption } = require('jasmine-spec-reporter');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { SpecReporter, StacktraceOption } = require("jasmine-spec-reporter");
 
 /**
  * @type { import("protractor").Config }
  */
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
+    specs: [
+        './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
     browserName: 'chrome'
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,19 +1,19 @@
-import { browser, element, by, ElementArrayFinder, ElementFinder} from 'protractor';
+import { browser, element, by, ElementArrayFinder, ElementFinder} from "protractor";
 
 export class AppPage{
     public async getDashboard(): Promise<any>{
-        return browser.get('/dashboard');
+        return browser.get("/dashboard");
     }
     public async getHeaderText(): Promise<string>{
-        const header = by.css('h2');
+        const header = by.css("h2");
         return element(header).getText();
     }
     public getListItems(): ElementArrayFinder{
-        const items = by.css('li');
+        const items = by.css("li");
         return element.all(items);
     }
     public getInput(): ElementFinder{
-        const input = by.css('input');
+        const input = by.css("input");
         return element(input);
     }
 }
