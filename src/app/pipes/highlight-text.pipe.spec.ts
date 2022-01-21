58780fd45d0eba7bbb4d094521f9c91410839974
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-21 19:46:30 +0300

    Added more unit tests for pipes

--- src/app/pipes/highlight-text.pipe.spec.ts
+++ src/app/pipes/highlight-text.pipe.spec.ts
@@ -10,4 +10,13 @@ describe('HighlightTextPipe', () => {
   it('wrap a simple string in a <span>', () => {
     expect(pipe.transform('foobar', 'foo')).toBe('<span class="highlight-text">foo</span>bar')
   });
+  it('wrap an uppercase string in a <span>', () => {
+    expect(pipe.transform('FOOBAR', 'foo')).toBe('<span class="highlight-text">FOO</span>BAR')
+  });
+  it('wrap a string with spaces in a <span>', () => {
+    expect(pipe.transform('FOO BAR', 'foo')).toBe('<span class="highlight-text">FOO</span> BAR')
+  });
+  it('wrap multiple words in a <span>', () => {
+    expect(pipe.transform('FOO FOO', 'foo')).toBe('<span class="highlight-text">FOO</span> <span class="highlight-text">FOO</span>')
+  });
 });
